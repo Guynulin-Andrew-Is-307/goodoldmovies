ca365e10cba15364a0c6e942c952be2788804534
Harufork <Harufork@users.noreply.github.com>
2020-10-08 13:10:43 +0500

    Initial commit

---
+++ goodilmovies/goodilmovies/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for goodilmovies project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goodilmovies.settings')
+
+application = get_asgi_application()
